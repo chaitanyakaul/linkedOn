1006f3e6817dbe3d004c6b2cc048cfab54848d28
chaitanya-nyllabs <chaitanya@nyllabs.tech>
2018-04-11 19:15:35 -0400

    Add ngRouter integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
